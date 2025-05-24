64eb895f326aca21e79d6f88199f3b6668575874
Build-1-0-0 <africancontent807@gmail.com>
2025-05-25 01:30:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build',
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: path.resolve(__dirname, 'index.html'),
       },
     },
-    outDir: 'dist',
   },
 });
