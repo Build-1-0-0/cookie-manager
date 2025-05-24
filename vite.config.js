55a01bf35fc7e667ce0c61ceb80098cdd0d14f61
Build-1-0-0 <africancontent807@gmail.com>
2025-05-25 01:06:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteTailwind } from 'vite-plugin-tailwindcss';
 
 export default defineConfig({
-  plugins: [react(), viteTailwind()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './frontend/public/index.html',
+      },
+    },
+    outDir: 'dist',
+  },
 });
